a11f78cf23a5f0f5ac9679a3abd650ff28587d4a
YiSha <yswhynot@gmail.com>
2016-04-27 22:06:56 +0800

    add

---
+++ header/MCTree.h
@@ -0,0 +1,22 @@
+/*
+ * MCTree.h
+ *
+ *  Created on: Apr 27, 2016
+ *      Author: yisha
+ */
+
+#ifndef MCTREE_H_
+#define MCTREE_H_
+
+class MCTree {
+public:
+	bool insertNode();
+	bool insertBranch();
+	bool insertGame();
+	int getTreeHeight();
+
+private:
+	int height;
+};
+
+#endif /* MCTREE_H_ */
---
+++ header/TraxRound.h
@@ -0,0 +1,39 @@
+/*
+ * TraxRound.h
+ *
+ *  Created on: Apr 27, 2016
+ *      Author: yisha
+ */
+
+#include <bitset>
+
+using namespace std;
+
+#ifndef TRAXSTATE_H_
+#define TRAXSTATE_H_
+
+struct TraxRound {
+	// One game play - unique id
+	long long int gameId;
+
+	// Current round number
+	// 0 for the initial state
+	// 1 for the 1st round
+	unsigned int roundIndex;
+
+	// Is me (1) or the opponent's turn (0) to play
+	bool isMe;
+
+	// Current board representation
+	// Count from top left to bottom right
+	// Horizontal first
+	// e.g.
+	/* 100 010 000
+	 * 000 110 000
+	 * 000 000 000
+	 */
+	// boardBitset = 100010000000110000000000000
+	bitset<1200> boardBitset;
+};
+
+#endif /* TRAXSTATE_H_ */
